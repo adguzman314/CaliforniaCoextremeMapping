fa46c8069c3b626bd430f4d5ab21194d3c9ed9bf
Anthony Guzman <167582076+adguzman314@users.noreply.github.com>
2025-08-07 20:20:41 -0700

    Update AirQualityDownloadScript.py

--- AirQualityDownloadScript.py
+++ AirQualityDownloadScript.py
@@ -1,14 +1,14 @@
 import requests
 
 def download_data_from_urls(urls, filenames):
-    # Record the start time
+    #Depending on connection, retriving all files for one sensor can take up to 10 minutes
     start_time = time.time()
 
     counter = 0
 
     for url in urls:
         response = requests.get(url)
-        # Check if the request was successful (status code 200)
+        #Check if the request was successful
         if response.status_code == 200:
             # Save the content of the response to a local CSV file
             with open(filenames[counter], "wb") as f:
@@ -16,12 +16,12 @@ def download_data_from_urls(urls, filenames):
             print("CSV file downloaded successfully")
         else:
             print("Failed to download CSV file. Status code:", response.status_code)
-        counter = counter + 1
+        counter =counter + 1
     counter = 0
     
     # Record the end time
     end_time = time.time()
 
-    # Calculate the duration
+    
     duration = end_time - start_time
     print("Task took", duration, "seconds")
